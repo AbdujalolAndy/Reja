6b81213d651724da69e4c5b70b0b144b2934d43b
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-09-23 19:05:02 +0900

    feat:

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require('express');
+const express = require("express");
 const http = require("http");
 const app = express();
 
 //1 Kirish code
-app.use(express.static('publuc'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //2 Sessions
+
 //3 Views code
 app.set("views", "views");
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //4 Routing code
-app.get('/hello', (req,res)=>{
-    res.end(`<h1>Hello World by Abdujalol</h1>`);
-});
-
-app.get('/gif', (req,res)=>{
-    res.end("<h1>Siz sovg\'alar bo\'limidasz</h1>");
+app.get('/',(req,res)=>{
+  res.render('harid.ejs')
 })
 
-const server  = http.createServer(app);
+app.post('/create-form',(req,res)=>{
+  console.log(req.body)
+  res.send(req.body)
+  // res.json({test:"success"})
+})
+
+const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, ()=>{
-    console.log(`The server is running successfully on port ${PORT}`)
+server.listen(PORT, () => {
+  console.log(`The server is running successfully on port: ${PORT}`);
 });
+
