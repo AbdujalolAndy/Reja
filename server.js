753099343181030dc211d83f64ac4b77bad6afb4
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-09-30 15:37:38 +0900

    feat:CRUD operations

--- server.js
+++ server.js
@@ -1,45 +1,21 @@
-const express = require("express");
 const http = require("http");
-const app = express();
-const fs = require('fs');
-let user;
-fs.readFile('database/user.json', 'utf8', (err,data)=>{
-  if(err){
-    console.log('Error:', err);
-  }else{
-    user=JSON.parse(data)
-  }
-})
+const mongodb = require('mongodb');
 
-//1 Kirish code
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const connectData = 'mongodb+srv://andy:DO2X0jYmpEjiSBBZ@cluster.clfkuth.mongodb.net/Reja';
 
-//2 Sessions
-
-//3 Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4 Routing code
-app.get('/',(req,res) => {
-  res.render('harid.ejs');
-})
-  // res.json({test:"success"})
-app.post('/create-form',(req,res) => {
-  console.log(req.body);
-  res.send(req.body);
-})
-
-app.get('/author', (req,res)=>{
-  res.render('author', {user:user})
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`The server is running successfully on port: ${PORT}`);
+mongodb.connect(connectData, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+        console.log('Error on connection MongoDB');
+    } else {
+        console.log('Successfully connected to MongoDB');
+        module.exports = client
+        const app = require('./app');
+        const server = http.createServer(app);
+        let PORT = 3000;
+        
+        server.listen(PORT, () => {
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+        });
+    }
 });
 
