fba0d6ce1519f78e8114ce46983a4dadeee57daf
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-10-03 02:52:27 +0900

    feat: New F-Task

--- train.js
+++ train.js
@@ -1,23 +1,25 @@
-// E-TASK: E-Task: Shunday function tuzing, u bitta string argumentni
-//  qabul qilib osh stringni teskari qilib return qilsin
+// 📌  F-Task: findDoublers function tuzing, unga faqat bitta string argument pass bolib,
+// agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+// masalan: getReverse("hello") return true return qiladi
 
-// masalan: getReverse("hello") return qilsin "olleh"
-const message = "Hello, this is me Andy";
+const txt = "hello";
 
-async function getReverse(txt) {
-  try {
-    if (txt == "string" || txt === "") throw new Error("Something went wrong with text");
-    else return txt.split("").reverse().join("");
-  } catch (error) {
-    throw error;
+async function findDoublers(txt) {
+  try{
+    if (typeof txt !== "string" || txt == "") throw new Error('Empty String');
+    else {
+      const count = {};
+      for (let letter of txt) {
+        count[letter] = (count[letter] ?? 0)+1
+        if(count[letter]>1) return true;
+      }
+      return false;
+    }
   }
+  catch(err){throw err;}
 }
 
-async function run() {
-  const reversedWord = await getReverse(message);
-  return reversedWord;
-}
-
-run()
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.message));
+findDoublers(txt)
+  .then((result)=>console.log("Text is doublers=>", result))
+  .catch((err)=>console.log('Error:', err));
+;
