3b869cfa0e6cacef0a350de4bd21fb1e50c1c230
Abdujalol, Andy <mrarnoldunlimited@gmail.com>
2023-10-03 00:56:30 +0900

    feat:Colorful terminal

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const http = require("http");
 const mongodb = require('mongodb');
+const colors = require('colors/safe')
 
 const connectData = 'mongodb+srv://andy:bav52w6vfTXIq5Cy@cluster0.4ahexgk.mongodb.net/Reja';
 
 
 mongodb.connect(connectData, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
-        console.log('Error on connection MongoDB');
+        console.log(colors.blue('Error on connection MongoDB'));
     } else {
-        console.log('Successfully connected to MongoDB');
+        console.log(colors.green('Successfully connected to MongoDB'));
         module.exports = client
         const app = require('./app');
         const server = http.createServer(app);
         let PORT = 3000;
         
         server.listen(PORT, () => {
-            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+            console.log(colors.rainbow(`The server is running successfully on port: ${PORT},`,), colors.red(`http://localhost:${PORT}`));
         });
     }
 });
